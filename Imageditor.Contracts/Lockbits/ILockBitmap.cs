79fe89a9c3297c452668c34d698f9025e5b6dffb
Benkő Gábor <benkogabor@msn.com>
2018-03-04 14:23:37 +0100

    created singuters for lockbitmap handler

---
+++ Imageditor.Contracts/Lockbits/ILockBitmap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imageditor.Contracts.Lockbits
+{
+    public interface ILockBitmap
+    {
+        void LockBits();
+
+        void UnlockBits();
+
+        Color GetPixel(int x, int y);
+
+        void SetPixel(int x, int y, Color color);
+    }
+}

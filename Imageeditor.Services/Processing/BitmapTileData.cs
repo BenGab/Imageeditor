b233a0d448176c30f908c932c9b5ac889aa4dcc4
Benkő Gábor <benkogabor@msn.com>
2018-04-02 20:52:14 +0200

    Thread base Image processing

---
+++ Imageeditor.Services/Processing/BitmapTileData.cs
@@ -0,0 +1,28 @@
+﻿using Imageditor.Contracts.Lockbits;
+using Imageditor.Contracts.Maybe;
+using System;
+
+namespace Imageeditor.Services.Processing
+{
+    public class BitmapTileData<T>
+    {
+        public int Startwidht { get; private set; }
+        public int Startheight { get; private set; }
+        public int Endwidht { get; private set; }
+        public int Endheight { get; private set; }
+        public ILockBitmap Bitmap { get; private set; }
+        public IMaybe<T> Value { get; private set; }
+        public Action<ILockBitmap, int, int, IMaybe<T>> Fn { get; private set; }
+
+        public BitmapTileData(int startwidth, int startHeight, int endwidth, int endheight, ILockBitmap bmp, IMaybe<T> value, Action<ILockBitmap, int, int, IMaybe<T>> converterFunction)
+        {
+            Startwidht = startwidth;
+            Startheight = startHeight;
+            Endwidht = endwidth;
+            Endheight = endheight;
+            Bitmap = bmp;
+            Value = value;
+            Fn = converterFunction;
+        }
+    }
+}
--- Imageeditor.Services/Processing/ImageProcessing.cs
+++ Imageeditor.Services/Processing/ImageProcessing.cs
@@ -2,7 +2,8 @@
 using System;
 using Imageditor.Contracts.Maybe;
 using Imageditor.Contracts.Lockbits;
-using System.Threading.Tasks;
+using System.Threading;
+using System.Collections.Generic;
 
 namespace Imageeditor.Services.Processing
 {
@@ -10,13 +11,39 @@ namespace Imageeditor.Services.Processing
     {
         public void AdjustImage<T>(ILockBitmap bitmap, IMaybe<T> value, Action<ILockBitmap, int, int, IMaybe<T>> converterFunction)
         {
-            Parallel.For(0, bitmap.Width, (i) =>
+            int tilewidth = bitmap.Width / 4;
+            int tileheight = bitmap.Height / 2;
+            List<Thread> threads = new List<Thread>();
+            for (int i = 0; i < 4; i++)
             {
-                for (int j = 0; j < bitmap.Height; j++)
+                int endwidth = (i + 1) * tilewidth;
+                for (int j = 0; j < 2; j++)
                 {
-                    converterFunction(bitmap, i, j, value);
+                    int endheight = (j + 1) * tileheight;
+                    var obj = new BitmapTileData<T>(i * tilewidth, j * tileheight, endwidth, endheight, bitmap, value, converterFunction);
+                    Thread th = new Thread(new ParameterizedThreadStart(AdjustFunction<T>));
+                    threads.Add(th);
+                    th.Start(obj);
                 }
-            });
+            }
+
+            foreach (var thread in threads)
+            {
+                thread.Join();
+            }
+        }
+
+        private void AdjustFunction<T>(object obj)
+        {
+            BitmapTileData<T> tiledata = (BitmapTileData<T>)obj;
+
+            for (int i = tiledata.Startwidht; i < tiledata.Endwidht; i++)
+            {
+                for (int j = tiledata.Startheight; j < tiledata.Endheight; j++)
+                {
+                    tiledata.Fn(tiledata.Bitmap, i, j, tiledata.Value);
+                }
+            }
         }
     }
 }
